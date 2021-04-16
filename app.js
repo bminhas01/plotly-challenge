b763cb520fcc5c54e22cbd0771d29c4559055818
Bismaad Minhas <bismaad@gmail.com>
2021-04-15 22:36:44 -0400

    text values updated for drop down options

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 d3.json("data/samples.json").then((Data) => {
-    console.log(Data)
-    var idOptions = Data.names
-    console.log(idOptions)
+    var idOptions = Data.names;
     idOptions.forEach((item) => {
         d3.select("#selDataset")
         .append("option")
         .attr("value", item)
-        .text(item)
-    })
-})
+        .text(item);
+    });
+});
