f593224c39e97ef66c3854f5068b130e6b9d399e
Bismaad Minhas <bismaad@gmail.com>
2021-04-15 22:34:13 -0400

    dropdown menu setup and options created

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 d3.json("data/samples.json").then((Data) => {
     console.log(Data)
+    var idOptions = Data.names
+    console.log(idOptions)
+    idOptions.forEach((item) => {
+        d3.select("#selDataset")
+        .append("option")
+        .attr("value", item)
+        .text(item)
+    })
 })
