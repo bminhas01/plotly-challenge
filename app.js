707b6315a6ba43d49bac4675b967ccccace62f1b
Bismaad Minhas <bismaad@gmail.com>
2021-04-16 14:08:38 -0400

    plotly bar graph created with fixed id value

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 d3.json("data/samples.json").then((importedData) => {
     // Create a variable to hold subject IDs
     // Populate dropdown menu with the id values
@@ -10,34 +9,57 @@ d3.json("data/samples.json").then((importedData) => {
         .text(item);
     });
 
-    var selectedID  = d3.select('#selDataset').node().value
-    
-    importedData.samples.forEach((row) =>{
-        Object.entries(row).forEach(([key, value])=>{
-            if (selectedID === value){
-                var filteredOtu_ids = row.otu_ids.slice(0,10)
-                console.log(filteredOtu_ids)
-                var filteredSample_values = row.sample_values.slice(0,10)
-                console.log(filteredSample_values)
-                var filteredOtu_labels = row.otu_labels.slice(0,10)
-                console.log(filteredOtu_labels)
-            }
-        })
-    })
-    
+    function init(){
+        var selectedID  = "940";
+        importedData.samples.forEach((row) =>{
+            Object.entries(row).forEach(([key, value])=>{
+                if (selectedID === value){
+                    var filteredOtu_ids = row.otu_ids.slice(0,10);
+                    var filteredSample_values = row.sample_values.slice(0,10);
+                    var filteredOtu_labels = row.otu_labels.slice(0,10);
 
+                    dataBar = [{
+                        x: filteredSample_values,
+                        y: filteredOtu_ids,
+                        text: filteredOtu_labels,
+                        type: "bar",
+                        orientation: 'h'
+                    }];
 
+                    var barLayout = {
+                    }
+            
+                    Plotly.newPlot("bar", dataBar, barLayout);
+                };
+            });
+        });
+    }
 
-    
+    d3.selectAll('#selDataset').on("change", updatePlotly);
 
-    // function filteredList(selectedID){
-    //     return importedData.metadata.id === selectedID
-    // }
-    
-    // d3.selectAll('#selDataset').on("change", updatePlotly);
-
-    // function updatePlotly(){
-
-    // }
+    function updatePlotly(){
+        var selectedID  = d3.select('#selDataset').node().value;
         
+        importedData.samples.forEach((row) =>{
+            Object.entries(row).forEach(([key, value])=>{
+                if (selectedID === value){
+                    var filteredOtu_ids = row.otu_ids.slice(0,10);
+                    var filteredSample_values = row.sample_values.slice(0,10);
+                    var filteredOtu_labels = row.otu_labels.slice(0,10);
+
+                    var xBar = [];
+                    var yBar = [];
+
+                    xBar = filteredSample_values,
+                    yBar = filteredOtu_ids,
+            
+                    Plotly.restyle("bar", "x", [xBar]);
+                    Plotly.restyle("bar", "y", [yBar]);
+                };
+            });
+        });
+    }
+
+init()
+
 })
